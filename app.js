af03d4428ad2e52c2429bc681161cf2cd1b22700
FreewayPvP <34444618+FreewayPvP@users.noreply.github.com>
2017-12-16 22:05:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,29 +12,46 @@ client.on('ready', () => {
 	client.user.setStatus('idle');
 });
 
-client.on('message', message => {
-	db.updateValue(message.author.id + message.guild.id, 1).then(i => {
+bot.on('message', message => {
 
-		let messages;
-		if (i.value == 25) messages = 25; // Level 1
-		if (i.value == 50) messages = 50; // Level 2
-		if (i.value == 75) messages = 75; // Level 3
-		if (i.value == 100) messages = 100; // Level 4
-		if (i.value == 125) messages = 125; // Level 5
-		if (i.value == 150) messages = 150; // Level 6
-		if (i.value == 175) messages = 175; // Level 7
-		if (i.value == 200) messages = 200; // Level 8
-		if (i.value == 225) messages = 225; // Level 9
-		if (i.value == 250) messages = 250; // Level 10
+    // Variables - Variables make it easy to call things, since it requires less typing.
+    let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
+    let sender = message.author; // This variable takes the message, and finds who the author is.
+    let args = message.content.slice(prefix.length).trim().split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
+    let cmd = args.shift().toLowerCase(); // This takes away the first object in the cont array, then puts it in this.
 
-		if (!isNaN(messages)) {
-			db.updateValue(`userLevel_${message.author + message.guild.id}`, 1).then(o => {
-				message.channel.send(`You Leveled Up!, You are now Level ${o.value}`)
-			})
+    // Message Leveling System - Make sure you require quick.db
+    db.updateValue(message.author.id + message.guild.id, 1).then(i => { // You pass it the key, which is authorID + guildID, then pass it an increase which is 1 in this instance.
+        // It also returns the new updated object, which is what we will use.
 
-		}
+        let messages; // Create an empty variable - These IF statements will run if the new amount of messages sent is the same as the number.
+        if (i.value == 25) messages = 25; // Level 1
+        else if (i.value == 50) messages = 50; // Level 2
+        else if (i.value == 100) messages = 100; // Level 3 - You can set these to any number, and any amount of them.
 
-	})
+        if (!isNaN(messages)) { // If messages IS STILL empty, run this.
+            db.updateValue(`userLevel_${message.author.id + message.guild.id}`, 1).then(o => { // This returns the updated object of userLevel_ID. 
+                message.channel.send(`You sent ${messages} messages, so you leveled up! You are now level ${o.value}`) // Send their updated level to the channel.
+            })
+        }
+
+    })
+
+    // We also need to make sure it doesn't respond to bots
+    if (sender.bot) return;
+    if (!message.content.startsWith(prefix)) return; // We also want to make it so that if the message does not start with the prefix, return.
+
+    // Command Handler - .trim() removes the blank spaces on both sides of the string
+    try {
+        let commandFile = require(`./commands/${cmd}.js`); // This will assign that filename to commandFile
+        commandFile.run(bot, message, args, func); // This will add the functions, from the functions.js file into each commandFile.
+    } catch (e) { // If an error occurs, this will run.
+        console.log(e.message); // This logs the error message
+    } finally { // This will run after the first two clear up
+        console.log(`${message.author.username} ran the command: ${cmd}`);
+    }
+
+});
 
 client.on('guildMemberAdd', member => {
    member.send("Welcome to our Server! I hope you Have FUN Getting Alts!\n|\nV\nV\nCommands:\n|\nV\n``!getMinecraft``\n``!getNetflix``\n``!getSpotify``");
