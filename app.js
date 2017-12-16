ba427d7ee13d82a008a8af47dea077e81a705ac9
FreewayPvP <34444618+FreewayPvP@users.noreply.github.com>
2017-12-16 21:46:27 +0100

    Added a Leveling System
    Leveling System until level 10!

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const Commando = require('discord.js-commando');
+const db = require('quick.db');
 
 const client = new Commando.Client({
 	owner: '367910644181368833'
@@ -11,6 +12,27 @@ client.on('ready', () => {
 	
 	client.user.setStatus('idle');
 });
+
+client.on('message', message => {
+	db.updateValue(message.author.id + message.guild.id, 1).then(i => {
+
+		let messages;
+		if (i.value == 25) messages = 25; // Level 1
+		if (i.value == 50) messages = 50; // Level 2
+		if (i.value == 75) messages = 75; // Level 3
+		if (i.value == 100) messages = 100; // Level 4
+		if (i.value == 125) messages = 125; // Level 5
+		if (i.value == 150) messages = 150; // Level 6
+		if (i.value == 175) messages = 175; // Level 7
+		if (i.value == 200) messages = 200; // Level 8
+		if (i.value == 225) messages = 225; // Level 9
+		if (i.value == 250) messages = 250; // Level 10
+
+		if (!isNaN(messages)) {
+			db.updateValue(`userLevel_${message.author + message.guild.id}`, 1).then(o => {
+				message.channel.send(`You Leveled Up!, You are now Level ${o.value}`)
+			})
+		}
 
 client.on('guildMemberAdd', member => {
    member.send("Welcome to our Server! I hope you Have FUN Getting Alts!\n|\nV\nV\nCommands:\n|\nV\n``!getMinecraft``\n``!getNetflix``\n``!getSpotify``");
